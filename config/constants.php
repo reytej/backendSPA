7534e07edd145f6d363887f99780e7f44108abf0
Enjo Santiago <enjo.santiago@yahoo.com>
2018-10-14 17:08:02 +0800

    constants - trans types

---
+++ config/constants.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: enjo
+ * Date: 14/10/2018
+ * Time: 4:58 PM
+ */
+
+return [
+    'trans_types' => [
+        'sales_invoice' => '10',
+        'credit_note' => '11',
+        'receipt' => '12',
+        'delivery' => '13',
+        'location_transfer' => '16',
+        'inventory_adjustment' => '17',
+        'purchase_order' => '18',
+        'supplier_invoice' => '20',
+        'supplier_credit_note' => '21',
+        'supplier_payment' => '22',
+        'po_delivery' => '25',
+        'sales_order' => '30',
+        'sales_quote' => '32',
+        'sales_quote' => '32',
+        'customer_refund' => '101'
+    ]
+];
